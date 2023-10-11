d2038a1dda0180b0583dd109d1bf05e5be651524
Astoon <86287036+Ast-Oscar@users.noreply.github.com>
2023-10-11 22:05:02 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Ast-Oscar.github.io',
-  base: '/Portfolio/',
+  site: 'https://Ast-Oscar.github.io/Portfolio/',
+  base: '/',
 })
